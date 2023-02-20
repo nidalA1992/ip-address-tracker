5c5b8282ec8fa3b89317c35093c265064b4d89d3
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-02-20 16:20:10 +0500

    make search-form separated component

--- src/features/app-header/ui/index.tsx
+++ src/features/app-header/ui/index.tsx
@@ -1,15 +1,18 @@
 import React, { useState } from "react";
 
 import s from "./styles.module.scss";
-import { SearchForm } from "./search-form";
 import { Display } from "./display";
+import { SearchForm } from "features/search-form";
 
 export const AppHeader = () => {
   const [positionData, setPositionData] = useState<IpGeolacationResponse>(
     {} as IpGeolacationResponse
   );
   return (
-    <header className={s.header}>
+    <header
+      className={s.header}
+      style={{ backgroundImage: 'url("pattern-bg.png")' }}
+    >
       <h1 className={s.title}>IP Address Tracker</h1>
       <SearchForm setData={setPositionData} />
       <Display {...positionData} />
--- src/features/app-header/ui/search-icon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const SearchIcon = () => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-      <path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6" />
-    </svg>
-  );
-};
